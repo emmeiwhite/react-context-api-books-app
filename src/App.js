5d1bc9123f642e177544efcef47452c50f1240e7
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-12-10 22:54:56 +0530

    Making axios request to add book with post request

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import { useState } from "react";
 import BookList from "./components/BookList";
 import BookCreate from "./components/BookCreate";
 
+import axios from "axios";
+
 function App() {
   const [books, setBooks] = useState([]);
 
   // function as prop to add the title [C of CRUD]
-  function createBook(title) {
-    setBooks([...books, { id: new Date().toISOString(), title }]);
+  async function createBook(title) {
+    /** 1) Before we re-render out UI with a state changes, let's first save the new book in our database */
+
+    const response = await axios.post("http://127.0.0.1:3001/books", {
+      title,
+    });
+
+    setBooks([...books, response.data]);
   }
 
   // deleting the item [D of CRUD] --- again passing the function as props
